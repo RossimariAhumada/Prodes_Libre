d0d04c7c2df1207915738b384b24812d41bff69f
RossimariAhumada <rossimari1998@gmail.com>
2024-08-28 13:53:35 -0500

    Se arregla banner dandole efecto de scroll

--- vista/modulo/inicio.php
+++ vista/modulo/inicio.php
@@ -32,16 +32,18 @@ include_once VISTA_PATH . 'encabezado.php';
             </div>
         </div>
     </section>
+
     <div class="banner">
-        <img src="<?php echo PUBLIC_PATH; ?>img/3.jpg" alt="Banner">
-        <div class="overlay">
-            <div class="text-center">
-                <h1 class="font-weight-bold">El camino hacia un futuro en equilibrio con nuestro planeta.</h1>
-                <p class="lead">Juntos, construimos un mundo donde las necesidades del presente no comprometen las del mañana.</p>
-                <a href="#" class="btn btn-color-btn btn-md">Ver más</a>
-            </div>
+    <div class="overlay"></div> <!-- Capa de fondo semitransparente -->
+    <div class="content">
+        <div class="text-center">
+            <h2 class="font-weight-bold text-white">El camino hacia un futuro en equilibrio con nuestro planeta.</h2>
+            <p class="lead text-white">Juntos, construimos un mundo donde las necesidades del presente no comprometen las del mañana.</p>
+            <!--<a href="#" class="btn btn-color-btn btn-md">Ver más</a>-->
         </div>
     </div>
+</div>
+
     <div class="container text-center my-5">
         <h2 class="text-color-btn fw-bold">ODS DESTACADOS</h2>
         <p>¡Prodes Libre: proyectos sostenibles, impacto global! Únete ahora y crea un futuro mejor.</p>
@@ -76,42 +78,6 @@ include_once VISTA_PATH . 'encabezado.php';
                         <div></div>
                     </a>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid bg-custom text-white py-5">
-        <div class="row">
-            <div class="col-md-4 text-center">
-                <img src="<?php echo PUBLIC_PATH; ?>img/Logo-PodresLibre_secundario.png" alt="PRODES LIBRE" class="img-fluid mb-3 logo-img">
-                <h5 class="mb-3">SÍGUENOS</h5>
-                <div class="d-flex justify-content-center">
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-facebook-f"><img src="<?php echo PUBLIC_PATH; ?>img/facebook.png" alt="search icon"></i>
-                    </a>
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-instagram"><img src="<?php echo PUBLIC_PATH; ?>img/instagram.png" alt="search icon"></i>
-                    </a>
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-twitter"><img src="<?php echo PUBLIC_PATH; ?>img/twitter.png" alt="search icon"></i>
-                    </a>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <h5>BARRANQUILLA, COLOMBIA</h5>
-                <p class="mb-0">
-                    Somos un semillero de investigación constituido por estudiantes de la facultad de ingeniería de la Universidad Libre seccional Barranquilla.
-                </p>
-            </div>
-            <div class="col-md-4">
-                <h5>CONTÁCTANOS</h5>
-                <p class="mb-0">
-                    <i class="fas fa-phone-alt"></i> Línea nacional gratuita<br>
-                    01 8000 180560<br>
-                    <i class="fas fa-map-marker-alt"></i> Sede carrera 46 - 48 -170<br>
-                    <i class="fas fa-envelope"></i> prodeslibre.baq@unilibre.edu.co<br>
-                    <i class="fas fa-map-marker-alt"></i> Barranquilla - Colombia
-                </p>
             </div>
         </div>
     </div>
--- vista/pie.php
+++ vista/pie.php
@@ -1,3 +1,39 @@
+<div class="container-fluid bg-custom text-white py-5">
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <img src="<?php echo PUBLIC_PATH; ?>img/Logo-PodresLibre_secundario.png" alt="PRODES LIBRE" class="img-fluid mb-3 logo-img">
+                <h5 class="mb-3">SÍGUENOS</h5>
+                <div class="d-flex justify-content-center">
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-facebook-f"><img src="<?php echo PUBLIC_PATH; ?>img/facebook.png" alt="search icon"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-instagram"><img src="<?php echo PUBLIC_PATH; ?>img/instagram.png" alt="search icon"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-twitter"><img src="<?php echo PUBLIC_PATH; ?>img/twitter.png" alt="search icon"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <h5>BARRANQUILLA, COLOMBIA</h5>
+                <p class="mb-0">
+                    Somos un semillero de investigación constituido por estudiantes de la facultad de ingeniería de la Universidad Libre seccional Barranquilla.
+                </p>
+            </div>
+            <div class="col-md-4">
+                <h5>CONTÁCTANOS</h5>
+                <p class="mb-0">
+                    <i class="fas fa-phone-alt"></i> Línea nacional gratuita<br>
+                    01 8000 180560<br>
+                    <i class="fas fa-map-marker-alt"></i> Sede carrera 46 - 48 -170<br>
+                    <i class="fas fa-envelope"></i> prodeslibre.baq@unilibre.edu.co<br>
+                    <i class="fas fa-map-marker-alt"></i> Barranquilla - Colombia
+                </p>
+            </div>
+        </div>
+    </div>
+
 <footer class="bg-danger text-white text-center py-3">
     <p class="mb-0">® Derechos reservados Semillero Prodes Libre 2023</p>
 </footer>
