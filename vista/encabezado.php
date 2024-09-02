89e697fd8ad6dba492d13456a1c43897269a1108
RossimariAhumada <rossimari1998@gmail.com>
2024-09-02 16:28:29 -0500

    se realiza correcion de menu y se agrega actividad

--- vista/encabezado.php
+++ vista/encabezado.php
@@ -1,55 +1,58 @@
-
-    <!-- Primera barra de navegación con el logo, barra de búsqueda, y iconos de usuario y carrito -->
-    <nav class="navbar navbar-light bg-white py-3">
-        <div class="container-fluid d-flex justify-content-between align-items-center">
-            <!-- Logo -->
-            <a class="navbar-brand d-flex align-items-center" href="#">
-                <img src="<?php echo PUBLIC_PATH; ?>img/Logo-PodresLibre.png" alt="logo">
-            </a>
-            <!-- Barra de búsqueda -->
-            <form class="d-flex flex-grow-1 justify-content-center mx-5">
-                <input class="form-control form-control-lg me-0" type="search" placeholder="Buscar.." aria-label="Search" style="width: 60%;">
-                <button class="btn btn-color-btn" type="submit">
-                    <img src="<?php echo PUBLIC_PATH; ?>img/lupa.png" alt="search icon" style="width: 24px;">
-                </button>
-                <!-- Iconos de carrito y usuario -->
-                <div class="d-flex align-items-center m-2">
-                    <img src="<?php echo PUBLIC_PATH; ?>img/bolsa.png" alt="Carrito" class="me-3" style="width: 24px;">
-                    <img src="<?php echo PUBLIC_PATH; ?>img/usuario.png" alt="Login" style="width: 24px;">
-                </div>
-        </div>
-        </form>
-    </nav>
-
-    <!-- Segunda barra de navegación con los enlaces -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-white" style="margin-top: -20px;height: 25px;">
-        <div class="container-fluid justify-content-center">
-            <!-- Botón para menú colapsable en dispositivos móviles -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+<!-- Primera barra de navegación con el logo, barra de búsqueda, y iconos de usuario y carrito -->
+<nav class="navbar navbar-light bg-white py-3">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+        <!-- Logo -->
+        <a class="navbar-brand d-flex align-items-center" href="#">
+            <img src="<?php echo PUBLIC_PATH; ?>img/Logo-PodresLibre.png" alt="logo" style="width: 250px;">
+        </a>
+        <!-- Barra de búsqueda -->
+        <form class="d-flex flex-grow-1 justify-content-center mx-5">
+            <input class="form-control form-control-lg me-0" type="search" placeholder="Buscar.." aria-label="Search" style="width: 65%; border-radius: 30px;">
+            <button class="btn btn-color-btn" type="submit" style="background-color: #C60909; border-radius: 30px;">
+                <img src="<?php echo PUBLIC_PATH; ?>img/lupa.png" alt="search icon" style="width: 24px;">
             </button>
-            <!-- Enlaces de navegación -->
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                <ul class="navbar-nav justify-content-center">
-                    <li class="nav-item">
-                        <a class="nav-link text-color-btn active" aria-current="page" href="<?php echo BASE_URL . 'inicio'; ?>">INICIO</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo BASE_URL . 'Interfaz' . DS . 'entrada_ods'; ?>">ODS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">ACTIVIDADES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">AVANCES</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">NOTICIAS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">CONTACTO</a>
-                    </li>
-                </ul>
-            </div>
+              <!-- Iconos de carrito y usuario -->
+        <div class="d-flex align-items-center m-2">
+            <img src="<?php echo PUBLIC_PATH; ?>img/bolsa.png" alt="Carrito" class="me-3" style="width: 30px;">
+            <img src="<?php echo PUBLIC_PATH; ?>img/usuario.png" alt="Login" style="width: 30px;">
         </div>
-    </nav>
+    </div>
+        </form>
+      
+</nav>
+
+<!-- Segunda barra de navegación con los enlaces -->
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container-fluid justify-content-center">
+        <!-- Botón para menú colapsable en dispositivos móviles -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Enlaces de navegación -->
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-color-btn active" aria-current="page" href="<?php echo BASE_URL . 'inicio'; ?>" style="color: #C60909; font-weight: bold;">INICIO</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo BASE_URL . 'Category' . DS . 'entrada_ods'; ?>" style="color: #333; font-weight: bold;">ODS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo BASE_URL . 'Category' . DS . 'actividades'; ?>" style="color: #333; font-weight: bold;">ACTIVIDADES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: #333; font-weight: bold;">AVANCES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: #333; font-weight: bold;">NOTICIAS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: #333; font-weight: bold;">BANCOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="color: #333; font-weight: bold;">CONTACTO</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ vista/modulo/category/actividades.php
@@ -0,0 +1,65 @@
+<?php
+include_once VISTA_PATH . 'encabezado.php';
+?>
+
+    <header class="bg-danger text-white text-center py-1">
+        <h3 class="display-5 fw-bold">ACTIVIDADES</h3>
+        <p>Objetivos y metas de desarrollo sostenible.</p>
+    </header>
+
+    <div class="custom-container">
+    <div class="inner-container">
+        <div class="custom-row">
+            <div class="custom-col">
+                <div class="custom-card">
+                    <a href="pagina-destino.html" class="custom-card-link">
+                        <img src="<?php echo PUBLIC_PATH; ?>img/Actividad_extraescolar.jpg" class="card-img-top" alt="Actividad">
+                    </a>
+                    <div class="custom-card-body">
+                        <h5 class="custom-card-title">ACTIVIDAD</h5>
+                        <p class="custom-card-text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de...</p>
+                        <a href="#" class="custom-card-link">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="custom-col">
+                <div class="custom-card">
+                    <a href="pagina-destino.html" class="custom-card-link">
+                        <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="Actividad">
+                    </a>
+                    <div class="custom-card-body">
+                        <h5 class="custom-card-title">ACTIVIDAD 2</h5>
+                        <p class="custom-card-text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de...</p>
+                        <a href="#" class="custom-card-link">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="custom-col">
+                <div class="custom-card">
+                    <a href="pagina-destino.html" class="custom-card-link">
+                        <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="Actividad">
+                    </a>
+                    <div class="custom-card-body">
+                        <h5 class="custom-card-title">ACTIVIDAD 3</h5>
+                        <p class="custom-card-text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de...</p>
+                        <a href="#" class="custom-card-link">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="custom-col">
+                <div class="custom-card">
+                    <a href="pagina-destino.html" class="custom-card-link">
+                        <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="Actividad">
+                    </a>
+                    <div class="custom-card-body">
+                        <h5 class="custom-card-title">ACTIVIDAD 4</h5>
+                        <p class="custom-card-text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de...</p>
+                        <a href="#" class="custom-card-link">Ver más</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+    </div>
+</div>
+ <?php include_once VISTA_PATH . 'pie.php';?>
+</body>
